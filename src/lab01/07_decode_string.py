f3669f39a0ff59c70cc7d8af176380e2c7260b03
lilylovesnobility <lilylilylily486@gmail.com>
2025-09-23 17:53:28 +0300

    07_decode_string.py

--- src/lab01/07_decode_string.py
+++ src/lab01/07_decode_string.py
@@ -1,12 +1,6 @@
 s = input().strip()
-
-# ищем первую заглавную букву -> начало
 start = next(i for i, ch in enumerate(s) if ch.isupper())
-
-# ищем цифру, после которой стоит второй символ
 digit_index = next(i for i, ch in enumerate(s) if ch.isdigit() and i + 1 < len(s))
-
-# шаг равен разнице между этими индексами
 step = (digit_index + 1) - start
 
 decoded = ""
